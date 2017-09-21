4f9e3be92131911168e246f94025b3db7e6f0064
Zyuhel <zyuhel@yandex.ru>
2017-09-21 12:42:27 +0300

    use accounts helper

--- logic/block.js
+++ logic/block.js
@@ -3,6 +3,7 @@
 var slots = require('../helpers/slots.js');
 var crypto = require('crypto');
 var bignum = require('../helpers/bignum.js');
+var accounts = require('../helpers/accounts.js');
 var ByteBuffer = require('bytebuffer');
 var BlockReward = require('../logic/blockReward.js');
 var constants = require('../helpers/constants.js');
@@ -49,15 +50,7 @@ __private.blockReward = new BlockReward();
  * @return {address} address
  */
 __private.getAddressByPublicKey = function (publicKey) {
-	var publicKeyHash = crypto.createHash('sha256').update(publicKey, 'hex').digest();
-	var temp = Buffer.alloc(8);
-
-	for (var i = 0; i < 8; i++) {
-		temp[i] = publicKeyHash[7 - i];
-	}
-
-	var address = bignum.fromBuffer(temp).toString() + 'L';
-	return address;
+	return accounts.getAddressByPublicKey(publicKey);
 };
 
 // Public methods
