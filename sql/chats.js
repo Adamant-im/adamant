6e9f384d9d7f7e54f63aa89c7efc5a5e06c5ef98
serg <sergushakov.public@gmail.com>
2018-12-26 19:50:00 +0300

    backward compatibility fixed #2

--- sql/chats.js
+++ sql/chats.js
@@ -23,7 +23,7 @@ var ChatsSql = {
         return [
             'SELECT COUNT(1) FROM full_blocks_list',
             (params.where.length ? 'WHERE ' + params.where.join(' AND ') : ''),
-            (params.whereOr.length ? 'AND (' + params.whereOr.join(' OR ') + ')': ''),
+            ((params.whereOr && params.whereOr.length) ? 'AND (' + params.whereOr.join(' OR ') + ')': ''),
             (params.sortField ? 'ORDER BY ' + [params.sortField, params.sortMethod].join(' ') : '')
         ].filter(Boolean).join(' ');
     },
@@ -46,7 +46,7 @@ var ChatsSql = {
         ].filter(Boolean).join(' ');
     },
     listChats: function (params) {
-        return [
+	    let x = [
             'SELECT *, t_timestamp as timestamp, ENCODE("publicKey", \'hex\') as "m_recipientPublicKey" FROM full_blocks_list',
             'LEFT OUTER JOIN mem_accounts ON address = "t_recipientId"',
             (params.where.length ? 'WHERE ' + params.where.join(' AND ') : ''),
@@ -54,6 +54,7 @@ var ChatsSql = {
             (params.sortField ? 'ORDER BY ' + [params.sortField, params.sortMethod].join(' ') : ''),
             'LIMIT ${limit} OFFSET ${offset}'
         ].filter(Boolean).join(' ');
+	    return x;
     },
 
 	getGenesis: 'SELECT b."height" AS "height", b."id" AS "id", t."senderId" AS "authorId" FROM trs t INNER JOIN blocks b ON t."blockId" = b."id" WHERE t."id" = ${id}'
