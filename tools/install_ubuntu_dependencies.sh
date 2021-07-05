7eeb15a4cc5e71312f3f9f78560aae4be38ddaf9
osmGosha <47744403+osmGosha@users.noreply.github.com>
2021-07-05 15:30:05 +0300

    Update node js version to 12

--- tools/install_ubuntu_dependencies.sh
+++ tools/install_ubuntu_dependencies.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y mc git curl python build-essential curl automake autoconf libtool redis-server
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get purge -y postgres*
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
@@ -8,4 +8,3 @@ wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key a
 sudo apt-get update
 sudo apt-get install -y postgresql postgresql-contrib libpq-dev
 sudo npm install -g bower grunt-cli pm2
-
