684f4c633ae32b4306011de472ae2fb69a8efa96
osmGosha <47744403+osmGosha@users.noreply.github.com>
2021-07-07 16:31:43 +0300

    Add new deps

--- tools/install_ubuntu_dependencies.sh
+++ tools/install_ubuntu_dependencies.sh
@@ -6,6 +6,5 @@ sudo apt-get purge -y postgres*
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y postgresql postgresql-contrib libpq-dev
+sudo apt-get install -y postgresql-12 postgresql-common postgresql-server-dev-12 libnode72 libpq5 libpq-dev
 sudo npm install -g bower grunt-cli pm2
-
