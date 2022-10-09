482e825978fe5d1176ac6e79f454227dfa595992
martiliones <hi@martilion.es>
2022-10-09 22:03:34 +0600

    fix: undefined migration file name, if it was not specified

--- tasks/newMigration.js
+++ tasks/newMigration.js
@@ -5,7 +5,17 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (grunt) {
+  /**
+   * Usage:
+   * ```
+   * npx grunt newMigration:YOUR_MIGRATION_NAME
+   * ```
+   */
   grunt.registerTask('newMigration', 'Create a new migration file.', function (name) {
+    if (!name) {
+      grunt.fail.fatal('The name is required.');
+    }
+
     var migration = {
       id: moment().format('YYYYMMDDHHmmss'),
       name: String(name)
