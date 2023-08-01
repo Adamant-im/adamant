84ce37a0a523503df14e0ea527db70e06dca90a6
adamant-al <adamantim@yandex.ru>
2023-08-01 12:32:38 +0600

    feat: add `-j` option to install scripts to set Nodejs version

--- tools/install_node.sh
+++ tools/install_node.sh
@@ -7,37 +7,48 @@ databasename="adamant_main"
 configfile="config.json"
 processname="adamant"
 port="36666"
+nodejs="hydrogen"
 
-while getopts 'b:n:' OPTION; do
-    OPTARG=$(echo "$OPTARG" | xargs)
-    case "$OPTION" in
-        b)
-            branch="$OPTARG"
-            ;;
-        n)
-            if [ "$OPTARG" == "testnet" ]
-            then
-                network="$OPTARG"
-                username="adamanttest"
-                databasename="adamant_test"
-                configfile="test/config.json"
-                processname="adamanttest"
-                port="36667"
-            elif [ "$OPTARG" != "mainnet" ]
-            then
-                printf "\nNetwork should be 'mainnet' or 'testnet'.\n\n"
-                exit 1
-            fi
-            ;;
-        *)
-            printf "\nWrong parameters. Use '-b' for branch, '-t' for network.\n\n"
-            exit 1
-        ;;
-    esac
+while getopts 'b:n:j:' OPTION; do
+  OPTARG=$(echo "$OPTARG" | xargs)
+  case "$OPTION" in
+    b)
+      branch="$OPTARG"
+      ;;
+    n)
+      if [ "$OPTARG" == "testnet" ]
+      then
+        network="$OPTARG"
+        username="adamanttest"
+        databasename="adamant_test"
+        configfile="test/config.json"
+        processname="adamanttest"
+        port="36667"
+      elif [ "$OPTARG" != "mainnet" ]
+      then
+        printf "\nNetwork should be 'mainnet' or 'testnet'.\n\n"
+        exit 1
+      fi
+      ;;
+    j)
+      if [ "$OPTARG" == "16" ] || [ "$OPTARG" == "gallium" ]
+      then
+        nodejs="gallium"
+      elif [ "$OPTARG" != "18" ] && [ "$OPTARG" != "hydrogen" ]
+      then
+        printf "\nNodejs should be 'gallium' = '16', or 'hydrogen' = '18'.\n\n"
+        exit 1
+      fi
+      ;;
+    *)
+      printf "\nWrong parameters. Use '-b' for branch, '-t' for network, '-j' for Nodejs version.\n\n"
+      exit 1
+    ;;
+  esac
 done
 
 printf "\n"
-printf "Welcome to the ADAMANT node installer v2.0.2 for Ubuntu 18, 20, 22. Make sure you got this file from adamant.im website or GitHub.\n"
+printf "Welcome to the ADAMANT node installer v2.1.0 for Ubuntu 18, 20, 22. Make sure you got this file from adamant.im website or GitHub.\n"
 printf "This installer is the easiest way to run ADAMANT node. We still recommend to consult IT specialist if you are not familiar with Linux systems.\n"
 printf "You can see full installation instructions on https://medium.com/adamant-im/how-to-run-your-adamant-node-on-ubuntu-990e391e8fcc\n"
 printf "The installer will ask you to set database and user passwords during the installation.\n"
@@ -45,51 +56,52 @@ printf "Also, the system may ask to choose some parameters, like encoding, keybo
 
 printf "Note: You've choosed '%s' network.\n" "$network"
 printf "Note: You've choosed '%s' branch.\n" "$branch"
+printf "Note: You've choosed '%s' Nodejs version.\n" "$nodejs"
 printf "\n"
 
 read -r -p "WARNING! Running this script is recommended for new droplets. Existing data MAY BE DAMAGED. If you agree to continue, type \"yes\": " agreement
 if [[ $agreement != "yes" ]]
 then
-    printf "\nInstallation cancelled.\n\n"
-    exit 1
+  printf "\nInstallation cancelled.\n\n"
+  exit 1
 fi
 
 IMAGE=false
 if [[ $network == "mainnet" ]]
 then
-    printf "\nBlockchain image saves time on node sync but you must completely trust the image.\n"
-    printf "If you skip this step, your node will check every single transaction, which takes time (up for several days).\n"
-    read -r -p "Do you want to use the ADAMANT blockchain image to bootstrap a node? [Y/n]: " useimage
-    case $useimage in
-        [yY][eE][sS]|[yY]|[jJ]|'')
-            IMAGE=true
-            printf "\nI'll download blockchain image and your node will be on the actual height in a few minutes.\n\n"
-        ;;
-        *)
-            printf "\nI'll sync your node from the beginning. It may take several days to raise up to the actual blockchain height.\n\n"
-        ;;
-    esac
+  printf "\nBlockchain image saves time on node sync but you must completely trust the image.\n"
+  printf "If you skip this step, your node will check every single transaction, which takes time (up for several days).\n"
+  read -r -p "Do you want to use the ADAMANT blockchain image to bootstrap a node? [Y/n]: " useimage
+  case $useimage in
+    [yY][eE][sS]|[yY]|[jJ]|'')
+      IMAGE=true
+      printf "\nI'll download blockchain image and your node will be on the actual height in a few minutes.\n\n"
+    ;;
+    *)
+      printf "\nI'll sync your node from the beginning. It may take several days to raise up to the actual blockchain height.\n\n"
+    ;;
+  esac
 fi
 
 hostname=$(cat "/etc/hostname")
 if grep -q "$hostname" "/etc/hosts"
 then
-    printf "Hostname /etc/hosts seems to be good.\n\n"
+  printf "Hostname /etc/hosts seems to be good.\n\n"
 else
-    printf "File /etc/hosts has no hostname record. I'll fix it.\n\n"
-    sh -c -e "echo '\n127.0.1.1    $hostname' >> /etc/hosts";
+  printf "File /etc/hosts has no hostname record. I'll fix it.\n\n"
+  sh -c -e "echo '\n127.0.1.1  $hostname' >> /etc/hosts";
 fi
 
 get_database_password () {
-    read -r -sp "Set the database password: $(echo $'\n> ')" postgrespwd
-    read -r -sp "$(echo $'\n')Confirm password: $(echo $'\n> ')" postgrespwdconfirmation
-    if [[ $postgrespwd = "$postgrespwdconfirmation" ]]
-    then
-        echo "$postgrespwd"
-    else
-        printf "\nPassword mismatch. Try again.\n\n"
-        get_database_password
-    fi
+  read -r -sp "Set the database password: $(echo $'\n> ')" postgrespwd
+  read -r -sp "$(echo $'\n')Confirm password: $(echo $'\n> ')" postgrespwdconfirmation
+  if [[ $postgrespwd = "$postgrespwdconfirmation" ]]
+  then
+    echo "$postgrespwd"
+  else
+    printf "\nPassword mismatch. Try again.\n\n"
+    get_database_password
+  fi
 }
 
 DB_PASSWORD="$(get_database_password)"
@@ -98,9 +110,9 @@ DB_PASSWORD="$(get_database_password)"
 printf "\n\nChecking if user '%s' exists…\n\n" "$username"
 if [[ $(id -u "$username" > /dev/null 2>&1; echo $?) = 1 ]]
 then
-    printf "Creating system user named '%s'…\n" "$username"
-    adduser --gecos "" "$username"
-    printf "User '%s' has been created.\n\n" "$username"
+  printf "Creating system user named '%s'…\n" "$username"
+  adduser --gecos "" "$username"
+  printf "User '%s' has been created.\n\n" "$username"
 fi
 
 #Packages
@@ -126,11 +138,11 @@ su - "$username" <<EOSU
 
 #NodeJS
 printf "\n\nInstalling nvm & node.js…\n\n"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 source ~/.nvm/nvm.sh
 source ~/.profile
 source ~/.bashrc
-nvm i --lts=gallium
+nvm i --lts=$nodejs
 npm i -g pm2
 
 #ADAMANT
@@ -144,10 +156,10 @@ printf "\n\nSetting node's config…\n\n"
 
 if [[ $configfile == "config.json" ]]
 then
-    cp default.config.json config.json
+  cp config.default.json config.json
 elif [ "$configfile" == "test/config.json" ]
 then
-    cp test/config.default.json test/config.json
+  cp test/config.default.json test/config.json
 fi
 
 rpl -i -q '"password": "password",' "\"password\": \"${DB_PASSWORD}\"," "$configfile"
@@ -158,30 +170,30 @@ rpl -i -q '"password": "password",' "\"password\": \"${DB_PASSWORD}\"," "$config
 # Download actual blockchain image for 'mainnet' network
 if [[ $IMAGE = true ]]
 then
-    printf "\n\nDownloading actual blockchain image…\n\n"
-    wget https://explorer.adamant.im/db_backup.sql.gz
-    printf "\nUnzipping the blockchain image, it can take a few minutes…\n\n"
-    gunzip db_backup.sql.gz
-    printf "\nLoading the blockchain image…\n\n"
-    psql adamant_main < db_backup.sql
-    printf "\nDeleting temporary blockchain image file…\n"
-    rm db_backup.sql
+  printf "\n\nDownloading actual blockchain image…\n\n"
+  wget https://explorer.adamant.im/db_backup.sql.gz
+  printf "\nUnzipping the blockchain image, it can take a few minutes…\n\n"
+  gunzip db_backup.sql.gz
+  printf "\nLoading the blockchain image…\n\n"
+  psql adamant_main < db_backup.sql
+  printf "\nDeleting temporary blockchain image file…\n"
+  rm db_backup.sql
 fi
 
 printf "\n\nAdding ADAMANT '%s' node to crontab for autostart after system reboot…\n\n" "$network"
 if [[ $network == "mainnet" ]]
 then
-    crontab -l | { cat; echo "@reboot cd /home/adamant/adamant && pm2 start --name adamant app.js"; } | crontab -
+  crontab -l | { cat; echo "@reboot cd /home/adamant/adamant && pm2 start --name adamant app.js"; } | crontab -
 else
-    crontab -l | { cat; echo "@reboot cd /home/adamanttest/adamant && pm2 start --name adamanttest app.js -- --config test/config.json --genesis test/genesisBlock.json"; } | crontab -
+  crontab -l | { cat; echo "@reboot cd /home/adamanttest/adamant && pm2 start --name adamanttest app.js -- --config test/config.json --genesis test/genesisBlock.json"; } | crontab -
 fi
 
 printf "\n\nRunning ADAMANT '%s' node…\n\n" "$network"
 if [[ $network == "mainnet" ]]
 then
-    pm2 start --name adamant app.js
+  pm2 start --name adamant app.js
 else
-    pm2 start --name adamanttest app.js -- --config test/config.json --genesis test/genesisBlock.json
+  pm2 start --name adamanttest app.js -- --config test/config.json --genesis test/genesisBlock.json
 fi
 
 EOSU
