089f229f0868aabef1074a297c17b5e1e33f6926
adamant-al <adamantim@yandex.ru>
2023-11-24 14:10:24 +0400

    debug: json-sql

--- logic/account.js
+++ logic/account.js
@@ -3,7 +3,7 @@
 var async = require('async');
 var pgp = require('pg-promise');
 var path = require('path');
-var jsonSql = require('json-sql')();
+var jsonSql = require('json-sql')({dialect: 'postgresql', namedValues: false});
 jsonSql.setDialect('postgresql');
 var constants = require('../helpers/constants.js');
 var slots = require('../helpers/slots.js');
