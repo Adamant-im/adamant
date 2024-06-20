75ae508d8a2fbda67270ff2c26248f0e959640cc
martiliones <hi@martilion.es>
2024-06-20 05:51:41 +0500

    fix: allow empty `types` and `types` without setting an address

--- modules/clientWs.js
+++ modules/clientWs.js
@@ -78,7 +78,12 @@ function findSubs (address1, address2, type, subs) {
   for (let aId in subs) {
     const sub = subs[aId];
     const {address, types, socket} = sub;
-    if ([address1, address2].includes(address) && types & (1 << n)) {
+    const isTypeAllowed = !types || types & (1 << n)
+
+    if (
+      ([address1, address2].includes(address) && isTypeAllowed) ||
+      isTypeAllowed
+    ) {
       filterred.push(socket);
     }
   }
