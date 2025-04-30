3a64ca1bca527f29319386b97029cf5d14bb0949
yoxira <m71262704@gmail.com>
2025-04-30 07:18:08 +0500

    feat: change count to number for `/transactions` and `/chatrooms/address` endpoints

--- modules/transactions.js
+++ modules/transactions.js
@@ -240,7 +240,7 @@ __private.list = function (filter, cb) {
     where: where,
     owner: owner
   }), params).then(function (rows) {
-    var count = rows.length ? Number(rows[0].count) : 0;
+    var count = rows.length ? rows[0].count : 0;
     var sql_method = 'list';
     if (filter.returnAsset) {
       sql_method = 'listFull';
@@ -274,7 +274,7 @@ __private.list = function (filter, cb) {
 
       var data = {
         transactions: transactions,
-        count: String(count + unconfirmedTransactions.length)
+        count: count + unconfirmedTransactions.length
       };
 
       return setImmediate(cb, null, data);
