e027f6baccb64b44263dc3fb908de353a31f1d82
yoxira <m71262704@gmail.com>
2025-04-30 07:33:31 +0500

    feat: return count as number for `/states/get` endpoint

--- sql/states.js
+++ sql/states.js
@@ -21,7 +21,7 @@ var StatesSql = {
   countList: function (params) {
     return [
 
-      'SELECT COUNT(1)  FROM full_blocks_list',
+      'SELECT COUNT(1)::INT FROM full_blocks_list',
       (params.where.length ? 'WHERE ' + params.where.join(' AND ') : ''),
       (params.sortField ? 'ORDER BY ' + [params.sortField, params.sortMethod].join(' ') : '')
     ].filter(Boolean).join(' ');
